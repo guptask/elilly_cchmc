f4023d09c053b11aec27e9b77b467b3492eb2500
Sounak Gupta <guptask@mail.uc.edu>
2015-08-14 15:39:42 -0400

    removed the need for plates per frame since for the last 2 plates, z layers have been merged

--- src/main.cpp
+++ src/main.cpp
@@ -19,8 +19,8 @@
 #define SOMA_FACTOR             1.5   // Soma factor
 #define COVERAGE_RATIO          0.4   // Coverage ratio lower threshold for neural soma
 #define PI                      3.14  // Approximate value of pi
-#define FRAMES_PER_WELL         20    // Frames per well
-#define PLATES_PER_FRAME        1     // Plates per frame
+#define FRAMES_PER_WELL         25    // Frames per well
+#define PLATES_PER_FRAME        15     // Plates per frame
 
 /* Channel type */
 enum class ChannelType : unsigned char {
@@ -861,15 +861,15 @@ int main(int argc, char *argv[]) {
 
     /* Process the image set */
     for (unsigned int well_index = 0; 
-            well_index < input_images.size()/(3*FRAMES_PER_WELL*PLATES_PER_FRAME); well_index++) {
+            well_index < input_images.size()/(3*FRAMES_PER_WELL); well_index++) {
 
         std::vector<float> aggregate;
         for (unsigned int combined_index = 0; 
-                combined_index < FRAMES_PER_WELL*PLATES_PER_FRAME; combined_index++) {
+                combined_index < FRAMES_PER_WELL; combined_index++) {
 
             // Process the 3-channel image
             unsigned int index = 
-                        3 * (FRAMES_PER_WELL*PLATES_PER_FRAME*well_index + combined_index);
+                        3 * (FRAMES_PER_WELL*well_index + combined_index);
             std::cout   << "Processing "
                         << input_images[index]      << ", "
                         << input_images[index+1]    << ", "
